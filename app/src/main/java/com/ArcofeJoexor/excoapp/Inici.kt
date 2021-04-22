d6cb7a1820ff7dbca0fdfe3365ecfef05683ee70
Arcofe <arcofe@bemen3.cat>
2021-04-22 17:48:10 +0200

    Initial commit

---
+++ app/src/main/java/com/ArcofeJoexor/excoapp/Inici.kt
@@ -0,0 +1,11 @@
+package com.ArcofeJoexor.excoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Inici: AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.inici)
+        }
+    }
