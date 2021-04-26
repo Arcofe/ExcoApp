571dcdf1e70f887f3f264fa95b6782097d69fed2
joexor <joexor@bemen3.cat>
2021-04-26 17:10:07 +0200

    Interficie Tema creada y funcionalidad boton atras

--- app/src/main/java/com/ArcofeJoexor/excoapp/TriarTema.kt
+++ app/src/main/java/com/ArcofeJoexor/excoapp/TriarTema.kt
@@ -1,11 +1,26 @@
 package com.ArcofeJoexor.excoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
-class TriarTema: AppCompatActivity() {
+class TriarTema: AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tema)
+        //declaracion botones
+        var atras = findViewById<ImageView>(R.id.atras)
+        atras.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.atras -> {
+                this.finish()
+            }
+        }
     }
 }
