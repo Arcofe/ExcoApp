6c2e4020fea5c77e4d4d58fb174471b59f0d292a
joexor <joexor@bemen3.cat>
2021-04-28 18:51:04 +0200

    Funcionalidad Google correcta y Principio de funcionalidad Registro

--- app/src/main/java/com/ArcofeJoexor/excoapp/Inici.kt
+++ app/src/main/java/com/ArcofeJoexor/excoapp/Inici.kt
@@ -1,15 +1,44 @@
 package com.ArcofeJoexor.excoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class Inici: AppCompatActivity() {
+class Inici: AppCompatActivity(), View.OnClickListener {
     enum class ProviderType{
         BASIC
     }
 
+    private var btnAjustes: ImageButton? = null
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.inici)
+
+            btnAjustes = findViewById<ImageButton>(R.id.ajustesInici)
+
+            /**
+             * Part on canviem dinamicament un TextView
+             */
+            var currentuser = Firebase.auth.currentUser
+            var usuario = findViewById<TextView>(R.id.usuario)
+            val user = currentuser?.email?.split('@')
+            usuario.text = user?.get(0)
+        }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.ajustesInici -> {
+                val intent = Intent(this, Ajustes::class.java)
+                startActivity(intent)
+            }
         }
     }
+}
--- app/src/main/java/com/ArcofeJoexor/excoapp/Registre.kt
+++ app/src/main/java/com/ArcofeJoexor/excoapp/Registre.kt
@@ -4,16 +4,79 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class Registre: AppCompatActivity(), View.OnClickListener {
+
+    private var btnRegistre: Button? = null
+    private var user: EditText? = null
+    private var password: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registre)
         //declaracion botones
         var btnVolverInicio = findViewById<Button>(R.id.btnBackLogIn)
         btnVolverInicio.setOnClickListener(this)
+
+        // Analytics Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("Message","Integració de Firebase completada")
+        analytics.logEvent("InitScreen",bundle)
+
+        //Setup
+        setup()
     }
+
+    private fun setup() {
+        /**
+         * Part per fer el Registre d'un usuari
+         */
+        btnRegistre?.setOnClickListener{
+            if (user?.text?.isNotEmpty()!! && password?.text?.isNotEmpty()!!){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(user!!.text.toString(),
+                    password!!.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", Inici.ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+
+                }
+            }
+        }
+    }
+
+    /**
+     * Classe d'alerta per quan tenim un error al iniciar sessió
+     */
+    private fun showAlert(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("S'ha produït un error en l'autentificació al usuari")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    /**
+     * Metode que introdueix usuaris per poder iniciar sessió, la quall guardara el usuari que sera el email i el password
+     */
+    private fun showHome(user: String, password: Inici.ProviderType){
+
+        val homeIntent  = Intent(this, Inici::class.java).apply {
+            putExtra("user",user)
+            putExtra("password",password.name)
+        }
+        startActivity(homeIntent)
+    }
+
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btnBackLogIn ->{
