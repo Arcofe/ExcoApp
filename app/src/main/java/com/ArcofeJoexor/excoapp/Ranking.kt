fdde844e1f0e47cf5075434c852e84f7f2c8ffa6
Arcofe <arcofe@bemen3.cat>
2021-04-28 20:12:24 +0200

    ExcoApp Ranking y conig_user

---
+++ app/src/main/java/com/ArcofeJoexor/excoapp/Ranking.kt
@@ -0,0 +1,12 @@
+package com.ArcofeJoexor.excoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Ranking: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ranking)
+    }
+}
---
+++ app/src/main/java/com/ArcofeJoexor/excoapp/User_Config.kt
@@ -0,0 +1,11 @@
+package com.ArcofeJoexor.excoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class User_Config : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_config)
+    }
+}
